7e12d96196a6d2945472a6f7eec0dd3be44ed34d
SwarupDas <78835981+swarupcs@users.noreply.github.com>
2023-08-20 09:12:50 +0530

    normal module in nodejs

---
+++ index.js
@@ -0,0 +1,3 @@
+const lib = require('./lib.js')
+
+console.log(lib.sum(4,5), lib.diff(9,6))
---
+++ lib.js
@@ -0,0 +1,9 @@
+function sum(a, b) {
+    return a+b;
+}
+
+exports.diff = (a, b) => {
+    return a-b;
+}
+
+exports.sum = sum
