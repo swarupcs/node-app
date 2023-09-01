d597cfdf8d777ca5b12705d527e53a9d60d4c9a5
SwarupDas <78835981+swarupcs@users.noreply.github.com>
2023-09-01 09:07:11 +0530

    commit

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const http = require('http');
+const fs = require('fs');
+
+const index = fs.readFileSync('index.html','utf-8');
 const data = { age : 5};
 const server = http.createServer((req, res) => {
     console.log(req.url);
     console.log('server started');
     res.setHeader('Dummy', 'DummyValue');
     res.setHeader('Content-Type', 'text/html');
-    res.end('hello')
+    res.end(index)
 });
 
 server.listen(8080)
